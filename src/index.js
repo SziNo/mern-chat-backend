9136bfd44f55fd99003c1945397a0ea632a8550d
SziNo <szino1848@gmail.com>
2025-03-30 18:48:11 +0200

    implement updateProfile and checkAuth

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
 
 import authRoutes from './routes/auth.route.js'
 import { connectDB } from './lib/db.js'
@@ -10,6 +12,8 @@ const app = express()
 const PORT = process.env.PORT || 5001
 
 app.use(express.json())
+app.use(cookieParser())
+// app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }))
 
 app.use('/api/auth', authRoutes)
 
