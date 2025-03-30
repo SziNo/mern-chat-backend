4b31080914a16a6434f0bbee79b01ec96e57c355
SziNo <szino1848@gmail.com>
2025-03-31 00:10:23 +0200

    implement message controller and route

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
 
-import authRoutes from './routes/auth.route.js'
 import { connectDB } from './lib/db.js'
+
+import authRoutes from './routes/auth.route.js'
+import messageRoutes from './routes/message.route.js'
 
 dotenv.config()
 const app = express()
@@ -16,6 +18,7 @@ app.use(cookieParser())
 // app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }))
 
 app.use('/api/auth', authRoutes)
+app.use('/api/message', messageRoutes)
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
