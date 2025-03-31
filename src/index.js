b98e65ea774ab08b53e2cafdb6b1640051c8f5dd
SziNo <szino1848@gmail.com>
2025-03-31 15:08:09 +0200

    increase limit of express.json

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const app = express()
 
 const PORT = process.env.PORT || 5001
 
-app.use(express.json())
+app.use(express.json({ limit: '3mb' }))
 app.use(cookieParser())
 app.use(cors({ origin: 'http://localhost:5173', credentials: true }))
 
