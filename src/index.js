9763bf87fa75f5106824bdffb96b800ba5c31cde
SziNo <szino1848@gmail.com>
2025-04-01 16:50:39 +0200

    update cors

--- src/index.js
+++ src/index.js
@@ -13,7 +13,15 @@ const PORT = process.env.PORT || 5001
 
 app.use(express.json({ limit: '3mb' }))
 app.use(cookieParser())
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }))
+app.use(
+  cors({
+    origin: [
+      'https://mern-chat-frontend-lemon.vercel.app/',
+      'http://localhost:5173',
+    ],
+    credentials: true,
+  })
+)
 
 app.use('/api/auth', authRoutes)
 app.use('/api/messages', messageRoutes)
