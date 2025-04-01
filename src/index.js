f6bbe2c20fdf7cbd08291ebf497136af3a25e13d
SziNo <szino1848@gmail.com>
2025-04-01 16:32:43 +0200

    implement socket.io

--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import express from 'express'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
-
 import { connectDB } from './lib/db.js'
-
 import authRoutes from './routes/auth.route.js'
 import messageRoutes from './routes/message.route.js'
+import { app, server } from './lib/socket.js'
 
 dotenv.config()
-const app = express()
 
 const PORT = process.env.PORT || 5001
 
@@ -20,7 +18,7 @@ app.use(cors({ origin: 'http://localhost:5173', credentials: true }))
 app.use('/api/auth', authRoutes)
 app.use('/api/messages', messageRoutes)
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
   connectDB()
 })
