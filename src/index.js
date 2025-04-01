5210d1194ac3de0c5e38ec51f51aa1a19e42e6c9
SziNo <szino1848@gmail.com>
2025-04-01 16:57:11 +0200

    remove slash at the end of the link

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ app.use(express.json({ limit: '3mb' }))
 app.use(cookieParser())
 app.use(
   cors({
-    origin: 'https://mern-chat-frontend-lemon.vercel.app/',
+    origin: 'https://mern-chat-frontend-lemon.vercel.app',
     credentials: true,
   })
 )
