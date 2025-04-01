c13f2b3202231577e760dba2622eae19920bee97
SziNo <szino1848@gmail.com>
2025-04-01 17:06:41 +0200

    update cors in socket.js too

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:5173'], // Replace this with deployed vercel frontend URL
+    origin: ['https://mern-chat-frontend-lemon.vercel.app'],
   },
 })
 
